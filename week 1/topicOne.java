747b71b1bb83c232b7a6c58712364ca137a31e4c
jiyasingh0987 <jiyasingh8383@gmail.com>
2026-09-07 10:27:49 +0530

    Day 1 - Variables and Data Types

---
+++ week 1/topicOne.java
@@ -0,0 +1,24 @@
+public class topicone {
+    public static void main(String[] args) {
+         
+        // variable and data types
+
+        // q1
+        String name = " jiya singh";
+        int age = 20;
+        double height = 5.2;
+        char grade = 'A';
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+
+        //q2
+        int length = 10;
+        int breadth = 5;
+        int area = length * breadth;
+        System.out.println("Area of rectangle: " + area);
+    }
+}
+
